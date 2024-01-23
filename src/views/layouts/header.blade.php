d428610c3e8093a427b8e53c2a0256af9a118638
marcosbd23 <marcosbrdr@gmail.com>
2024-01-23 19:36:37 +0000

    Navbar changed to a better one

--- src/views/layouts/header.blade.php
+++ src/views/layouts/header.blade.php
@@ -1,11 +1,16 @@
-<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="{{$router->generate('index')}}">iFriend</a>
-    <a class="navbar-brand" href="{{$router->generate('user')}}">Usuarios</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-primary" type="submit">Search</button>
-    </form>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="{{$router->generate('user')}}">Usuarios</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 {{-- @include("layouts.alert") --}}
