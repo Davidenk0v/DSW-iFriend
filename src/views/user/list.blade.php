dd71a3a041df80a36bc2ce6614c306a5237a19d0
marcosbd23 <marcosbrdr@gmail.com>
2024-01-23 19:28:04 +0000

    que hace subnormal

--- src/views/user/list.blade.php
+++ src/views/user/list.blade.php
@@ -4,17 +4,7 @@
 
 @section("content")
 <div class="d-flex flex-row-reverse">
-<<<<<<< HEAD
   <a class="btn btn-primary" href="{{$router->generate('user_create')}}"><i class="bi bi-person-plus-fill"></i> Crear Usuario</a>
-=======
-    <a href="{{$router->generate('user_create')}}" class="d-inline-flex m-2 justify-content-center align-items-center btn btn-primary" href="#">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="m-1" viewBox="0 0 16 16">
-        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
-      </svg>
-      Crear nuevo usuario
-    </a>
->>>>>>> 3f0a691c6b9fc3a74c5d0dbe24fbfe714e21bf06
 </div>
 <table class="table table-striped">
   <thead>
@@ -36,21 +26,8 @@
         <td>{{$user->created_at}}</td>
         <td>{{$user->updated_at}}</td>
         <td>
-<<<<<<< HEAD
           <a class="btn btn-warning" href="{{$router->generate('user_edit', ["id" => $user->id])}}"><i class="bi bi-pencil-fill"></i></a>
           <a class="btn btn-danger" href="{{$router->generate('user_delete', ["id" => $user->id])}}"><i class="bi bi-trash-fill"></i></a>
-=======
-          <a class="d-inline-flex justify-content-center align-items-center btn btn-warning" href="{{$router->generate('user_edit', ["id" => $user->id])}}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-            </svg>
-          </a>
-          <a class="d-inline-flex justify-content-center align-items-center btn btn-danger" href="{{$router->generate('user_delete', ["id" => $user->id])}}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
-            </svg>
-          </a>
->>>>>>> 3f0a691c6b9fc3a74c5d0dbe24fbfe714e21bf06
         </td>
       </tr>
     @empty
